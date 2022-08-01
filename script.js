c99cea8a6581972a7d8370f42c4a31e183d0a8a3
Oscar Luis Yopan <oscar.yopan@outlook.com>
2022-08-01 16:19:03 -0300

    Done

--- script.js
+++ script.js
@@ -41,9 +41,16 @@ function setTime() {
   date.innerHTML = `${days[day]}, ${day} de ${months[month]} de ${year}`
 }
 
+
+/*------------------*/
+// Reference:
+// StackOverflow https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
+
 const scale = (num, in_min, in_max, out_min, out_max) => {
   return (num - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
+
+/*------------------*/
 
 setTime()
 
